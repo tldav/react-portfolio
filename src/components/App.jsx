d4d7df48f7038a4513cd23bed240f6bbe6d6bb5c
Neverender <tldavis09@yahoo.com>
2020-05-21 21:09:15 -0500

    basic sekelton. Nothing done yet

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+	state = {};
+	render() {
+		return (
+			<div>
+				<h3>hello</h3>
+			</div>
+		);
+	}
+}
+
+export default App;
